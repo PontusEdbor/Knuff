3b32c98f430c37ab8b5a1c9991e338b218b435ce
pdbr <pontusedbor>
2017-11-17 15:03:16 +0100

    Added a basic Map class

---
+++ Map.java
@@ -0,0 +1,19 @@
+public class Map{
+    private int[][] tiles; //Starting out with
+    
+    public Map(){
+	this.tiles = new int[9][16]; //Starting out small with hardcoded values
+    }
+    public void render(){
+	for(int[] row : this.tiles){
+	    for(int tile : row){
+		System.out.print(tile);
+	    }
+	    System.out.println();
+	}
+    }
+    public static void main(String[] args){
+	Map m = new Map();
+	m.render();
+    }
+}
