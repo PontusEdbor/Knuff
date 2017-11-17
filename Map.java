a356ddc24307dbbc33df0f89b34047ee5294dd05
pdbr <pontusedbor>
2017-11-17 15:05:05 +0100

    addded some comments

--- Map.java
+++ Map.java
@@ -2,7 +2,7 @@ public class Map{
     private int[][] tiles; //Starting out with
     
     public Map(){
-	this.tiles = new int[9][16]; //Starting out small with hardcoded values
+	this.tiles = new int[9][16]; //Starting out small with hardcoded values [y][x]
     }
     public void render(){
 	for(int[] row : this.tiles){
