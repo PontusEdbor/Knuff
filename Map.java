538ed5da7d38accbcf47e7a180e954c079f49fab
pdbr <pontusedbor>
2017-11-17 15:07:45 +0100

    changed the int matrix to a char matrix

--- Map.java
+++ Map.java
@@ -1,12 +1,12 @@
 public class Map{
-    private int[][] tiles; //Starting out with
+    private char[][] tiles; //Starting out with representing them with chars
     
     public Map(){
-	this.tiles = new int[9][16]; //Starting out small with hardcoded values [y][x]
+	this.tiles = new char[9][16]; //Starting out small with hardcoded values [y][x]
     }
     public void render(){
-	for(int[] row : this.tiles){
-	    for(int tile : row){
+	for(char[] row : this.tiles){
+	    for(char tile : row){
 		System.out.print(tile);
 	    }
 	    System.out.println();
